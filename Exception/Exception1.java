ec49dc262cc637f6fb759f1714680c057c349030
uthso297 <uthso119464@gmail.com>
2024-12-09 23:45:38 +0600

    practiced exception 1

---
+++ Exception/Exception1.java
@@ -0,0 +1,16 @@
+package Exception;
+
+public class Exception1 {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 0;
+
+        try {
+            j = 18 / i;
+        } catch (Exception e) {
+            System.out.println("Something went wrong " + e);
+        }
+        System.out.println(j);
+        System.out.println("Bye");
+    }
+}
